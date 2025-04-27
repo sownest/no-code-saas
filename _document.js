2d5cdb44c958de4a3dc8c26c18dfc4c0c37b6e42
sownest <sownest.real@gmail.com>
2025-04-27 05:47:55 -0700

    Fixed issues version6

---
+++ _document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+        <meta name="google-site-verification" content="Qte4WLs_77BK26KBwKx4SbPxQzWuBUwUHuBdi_u2uc0" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
